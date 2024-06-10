947755e04d26aed33f0cf27123f796be20fd9da4
CharlesSampaio-CRS <ccharlesroberto@gmail.com>
2024-06-10 00:08:33 -0300

    Add git ignore

---
+++ contracts/class/PersonDataStorage.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+contract PersonDataStorage {
+
+    string public name = "UserName";
+    uint private age = 35;
+    bool internal isBrazilian = true;
+
+    constructor ( ){
+
+    }
+    
+}
