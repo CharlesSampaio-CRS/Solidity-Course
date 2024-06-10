e64f89efd8503cb8db53c197c88823a73d4b953b
CharlesSampaio-CRS <ccharlesroberto@gmail.com>
2024-06-10 08:37:41 -0300

    Add functions basics

--- contracts/class/PersonDataStorage.sol
+++ contracts/class/PersonDataStorage.sol
@@ -3,12 +3,43 @@ pragma solidity 0.8.26;
 
 contract PersonDataStorage {
 
-    string public name = "UserName";
-    uint private age = 35;
-    bool internal isBrazilian = true;
+    string public name;
+    uint private age;
+    bool internal isBrazilian;
 
     constructor ( ){
-
+        name ="UserName";
+        age = 19;
+        isBrazilian = true;
     }
-    
+
+    function setName(string memory _name) public {
+        name = _name;
+    }
+
+    function setAge(uint _age) public {
+        age = _age;
+    }
+
+    function setIsBrazilian(bool _brazilian) public {
+        isBrazilian = _brazilian;
+    }
+
+
+    function getAge() public view returns (uint){
+        return age;
+    }
+
+    function getIsBrazilian() public view returns(bool){
+        return isBrazilian;
+    }
+
+    function addValues(uint _v1, uint _v2) pure  internal  returns (uint){
+        return _v1 + _v2;
+    }
+
+    function incrementAge() external {
+        age = addValues(age, 1);
+    }
+
 }
